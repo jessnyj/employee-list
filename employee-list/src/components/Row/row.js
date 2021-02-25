8a3082b581118d067d7238ce517f62048f68bb92
jessny <jessnyjoseph@gmail.com>
2021-02-25 09:39:22 -0600

    updated rowjs with variables accessing apu

--- employee-list/src/components/Row/row.js
+++ employee-list/src/components/Row/row.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./row.css";
 
-function Row() {
+function Row({name, email, image, phone, dob}) {
     return (
         <table class="table table-striped">
             <tbody>
                 <tr>
                     <th scope="row"></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>{name}</td>
+                    <td>{email}</td>
+                    <td>{image}</td>
+                    <td>{phone}</td>
+                    <td>{dob}</td>=
                 </tr>
             </tbody>
         </table>
