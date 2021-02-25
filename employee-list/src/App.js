6f0ad433fde1360de61f779cba08b99d40d1f0fb
jessny <jessnyjoseph@gmail.com>
2021-02-24 23:10:18 -0600

    added use effect and load users functions

--- employee-list/src/App.js
+++ employee-list/src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
-// import Wrapper from "./components/Wrapper/wrapper";
+import React, { useEffect } from "react";
 import Jumbotron from "./components/Jumbotron/jumbotron";
 import Search from "./components/Search/search";
 import Table from "./components/Table/table";
+import API from "./utils/API";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    loadUsers()
+  });
+
+  const loadUsers = (user) => {
+    API.getUsers(user).then((users) => {
+      setUsers(users)
+    })
+    .catch(err => console.log(err));
+  };
+
+
   return(
     <div>
       <Jumbotron />
