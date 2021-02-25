1407710bf5fc61cdc71084b952f8f395ff9f8354
jessny <jessnyjoseph@gmail.com>
2021-02-25 10:13:25 -0600

    trying to add fontawesome icon

--- employee-list/src/components/Header/header.js
+++ employee-list/src/components/Header/header.js
@@ -7,7 +7,7 @@ function Header() {
             <thead>
                 <tr>
                     <th scope="col">Image</th>
-                    <th scope="col">Name</th>
+                    <th scope="col">Name <i class="fas fa-sort-down"></i></th>
                     <th scope="col">Phone</th>
                     <th scope="col">Email</th>
                     <th scope="col">DOB</th>
